12ed7c005ee2b80f2b0c31c7c00f43f112bbad41
Meagh Bent <thevariables@gmail.com>
2015-05-24 21:37:06 -0400

    Yeoman generated boilerplate ready to roll locally.

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('ttchelperApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
